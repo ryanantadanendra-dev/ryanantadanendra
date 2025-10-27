6b3fbf62d6f3fe07f8a285f49c4f466f41a8545f
Ryananta Danendra <ryanantadanendra887@gmail.com>
2025-10-27 15:27:46 +0800

    Fixing Navbar.tsx prev data type

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -22,7 +22,7 @@ function Navbar({ className }: { className?: string }) {
   const { scrollY } = useScroll();
 
   useMotionValueEvent(scrollY, "change", (latest) => {
-    const prev = scrollY ? scrollY?.getPrevious() : 0;
+    const prev = scrollY ? scrollY.getPrevious() ?? 0 : 0;
     if (latest > prev && latest > 150) {
       setHidden(true);
     } else {
