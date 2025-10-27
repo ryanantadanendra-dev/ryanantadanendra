c52823b85eb2c519262dc1235f48ab53bb2096da
Ryananta Danendra <ryanantadanendra887@gmail.com>
2025-10-27 11:41:48 +0800

    Fixing fonts.ts route

--- src/app/fonts.tsx
+++ src/app/fonts.tsx
@@ -3,12 +3,12 @@ import localFont from "next/font/local";
 export const raleway = localFont({
   src: [
     {
-      path: "./fonts/Raleway//static/Raleway-Bold.ttf",
+      path: "./fonts/Raleway/static/Raleway-Bold.ttf",
       weight: "500",
       style: "normal",
     },
     {
-      path: "./fonts/Raleway//static/Raleway-Regular.ttf",
+      path: "./fonts/Raleway/static/Raleway-Regular.ttf",
       weight: "400",
       style: "normal",
     },
