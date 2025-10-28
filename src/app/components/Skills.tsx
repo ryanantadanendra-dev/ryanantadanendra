55f33ecbd54f4c61726efab829d68281c952be50
Ryananta Danendra <ryanantadanendra887@gmail.com>
2025-10-28 18:06:29 +0800

    Fixing Image viewpoer attribute

--- src/app/components/Skills.tsx
+++ src/app/components/Skills.tsx
@@ -104,7 +104,7 @@ const Skills = () => {
           initial={{ y: 500, scale: 0, rotate: 180 }}
           whileInView={{ y: 0, scale: 1, rotate: 0 }}
           transition={{ duration: 0.8, ease: "easeInOut" }}
-          viewport={{ once: true, amount: 0.2 }}
+          viewport={{ once: true, amount: 0.2, margin: "500px 0px 500px 0px" }}
         >
           <h2 className="text-black mt-4 ms-4">Back-end</h2>
           <motion.div
@@ -161,6 +161,7 @@ const Skills = () => {
           viewport={{
             once: true,
             amount: 0.2,
+            margin: "500px 0px 500px 0px",
           }}
         >
           <h2 className="text-black mt-4 ms-4">Tools</h2>
@@ -234,7 +235,7 @@ const Skills = () => {
             initial={{ y: isMobile ? 10 : 90 }}
             whileInView={{ y: 0 }}
             transition={{ delay: 1, duration: 0.5, ease: "easeOut" }}
-            viewport={{ once: true, amount: 0.3 }}
+            viewport={{ once: true }}
             className="skill-image absolute -bottom-6 lg:-right-12 md:-right-14 -rotate-12"
           >
             <Image
